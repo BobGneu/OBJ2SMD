6c2e473b7ad4348742839d3e0b0b31f503189e2d
Bob Chatman <bob.chatman@gmail.com>
2018-01-06 21:55:34 -0800

    Adding entry point and usage/args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate docopt;
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+OBJ Converter
+
+Usage:
+  obj2smd.exe [--report] <source>...
+  obj2smd.exe (-z | --zup) [--report] <source>...
+  obj2smd.exe (-h | --help)
+  obj2smd.exe --version
+
+Options:
+  -h --help    Show this screen.
+  --version    Show version.
+  -z --zup     Source is zUp 
+  --report     Show the statistics report of the conversion
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_zup: bool,
+    flag_report: bool,
+    arg_source: Vec<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.deserialize())
+                            .unwrap_or_else(|e| e.exit());
+
+    println!("{:?}", args);
 }
