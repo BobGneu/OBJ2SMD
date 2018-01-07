5ff548014af473abd8e5bff04d9f76986828ae18
Bob Chatman <bob.chatman@gmail.com>
2018-01-07 00:44:58 -0800

    Finalizing standard arguments

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,30 @@ const USAGE: &'static str = "
 OBJ Converter
 
 Usage:
-  obj2smd.exe [--report] <source>...
-  obj2smd.exe (-z | --zup) [--report] <source>...
+  obj2smd.exe [-z | --zup] [--report] <source> [--image=<path>]...
   obj2smd.exe (-h | --help)
   obj2smd.exe --version
 
 Options:
-  -h --help    Show this screen.
-  --version    Show version.
-  -z --zup     Source is zUp 
-  --report     Show the statistics report of the conversion
+  -h --help         Show this screen
+  --version         Show version
+  --image=<path>    Add a tga to the listing
+  -z --zup          Source mesh is zUp 
+  --report          Show the statistics report of the conversion
 ";
 
 #[derive(Debug, Deserialize)]
 struct Args {
     flag_zup: bool,
     flag_report: bool,
-    arg_source: Vec<String>
+    flag_image: Vec<String>,
+    arg_source: String
 }
 
 fn main() {
     let args: Args = Docopt::new(USAGE)
-                            .and_then(|d| d.deserialize())
-                            .unwrap_or_else(|e| e.exit());
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
 
     println!("{:?}", args);
 }
